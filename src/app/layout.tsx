f74641cc46c931c6d256030bc6e94973ab094ca3
prashant <codecode427@gmail.com>
2025-09-13 15:32:05 +0530

    final fix seem like

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
-import { Toaster } from "@/components/ui/sonner"
-import Navbar from "@/components/Navbar";
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Create Next App",
